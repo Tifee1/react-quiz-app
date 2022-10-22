590495c2fa29b6591db6a7888e417c669e057d81
Tifee <boludami238@gmail.com>
2022-10-22 13:23:41 +0100

    edited github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 const Footer = () => {
   return (
     <footer className='bg-[#292929] text-grey text-center capitalize py-4 text-xl'>
-      developed by tifee
+      developed by{' '}
+      <a
+        href='https://github.com/tifee1'
+        target='_blank'
+        className='font-black uppercase text-white'
+      >
+        tifee
+      </a>
       <span className='text-white'> &copy; {new Date().getFullYear()}</span>
     </footer>
   )
